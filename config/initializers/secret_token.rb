26d2a4b52590e8a4083cc5377b899b7b0c69da6c
Zhenming Yuan <zhenming@ZhenmingtekiMacBook-Pro.local>
2014-01-20 16:22:10 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myzombies::Application.config.secret_key_base = 'c01971ff23b0152bffd23b867cabd8370e02d5059b74874c03948985dc0ed056ec73b53b2c38e8a1ac599d871a3a52b829a3b12c3071587672c4d92f4da49f80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myzombies::Application.config.session_store :cookie_store, key: '_myzombies_session'
